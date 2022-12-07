0b4bd9e1965f344cd93fab367b27dad7d976312c
Tian Meh <tian.meh99@gmail.com>
2022-12-07 13:37:02 +0100

    fixing

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.eventNames.DATABASE_URL,
-      port: 3000,
+      // port: 3000,
       ssl: true
     }
 });
