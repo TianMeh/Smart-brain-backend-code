0d52c2d807ef88269f18c1a7ce4987a555cbc982
Tian Meh <tian.meh99@gmail.com>
2022-12-07 08:10:37 +0100

    main files

---
+++ controllers/image.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '0b0cf7318e1b4147a5b6f3e56d59d7e0'
+});
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
