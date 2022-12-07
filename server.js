2c1d24fbb00b553af8123c063b83196a8ea7dfa7
Tian Meh <tian.meh99@gmail.com>
2022-12-07 13:28:13 +0100

    fixing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : process.eventNames.DATABASE_URL,
+      connectionString: process.eventNames.DATABASE_URL,
       // port : process.env.PORT || 3000,
       ssl: {
         rejectUnauthorized: false
